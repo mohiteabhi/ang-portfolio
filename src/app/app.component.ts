bae9b79007d63355b7b7637e6291b8ec096ccbfd
mohiteabhi <mohiteabhijeet74@gmail.com>
2025-04-15 13:33:02 +0530

    disable right click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,21 @@ export class AppComponent implements OnInit{
   originalTitle = 'Abhijeet Mohite | Portfolio';
 
   ngOnInit(): void {
+
+    document.addEventListener('contextmenu', (event) => {
+      event.preventDefault();
+    });
+
+    document.addEventListener('keydown', (event) => {
+      if (
+        event.key === "F12" ||
+        (event.ctrlKey && event.shiftKey && (event.key === "I" || event.key === "C")) ||
+        (event.ctrlKey && event.key === "U")
+      ) {
+        event.preventDefault();
+      }
+    });
+
     document.addEventListener('visibilitychange', () => {
       if (document.visibilityState === 'hidden') {
         document.title = 'Come back to Portfolio 😃';
@@ -21,4 +36,8 @@ export class AppComponent implements OnInit{
       document.title = "See Abhijeet's Portfolio Again 🥺";
     });
   }
+
+  
+
+  
 }
